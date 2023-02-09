22725b76e2821e312dfd573158ecb63a4f6c8781
administrador <administrador@LUBUNTU-SISTEMA2>
2023-02-09 15:34:33 +0100

    test

---
+++ home/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package M9UF3_Sockets_y_Servicios.home;
+
+/**
+ *
+ * @author administrador
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
